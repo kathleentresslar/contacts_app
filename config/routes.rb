c2968148d0781ee86bb6e6a6a35c66ee69fd518b
KathleenTresslar <kathleen.tresslar@gmail.com>
2019-06-27 20:21:05 -0500

    adding RESTful methods to contacts app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+    get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
+    patch "/contacts/:id" => "contacts#update"
+    delete "/contacts/:id" => "contacts#destroy"
+  end
+end
